4b6fa54470a427c74279017fe6283fcb41a53b62
tahir <tahir.monir@gmail.com>
2021-03-11 15:35:48 -0600

    re-arranged

---
+++ DBFirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DBFirstApp.Models;
+using System.Collections.Generic;
+
+namespace DBFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var dbcontext = new masterContext())
+            {
+                IEnumerable<Customer> customers = dbcontext.Customers;
+
+                foreach(var c in customers)
+                    System.Console.WriteLine(c.FirstName);
+            }
+        }
+    }
+}
